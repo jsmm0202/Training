831145430104b62cdb4df3ec3e76a1f7675ff4a1
JS_MM <jsmm0202@gmail.com>
2020-03-23 17:03:48 +0200

    Identifying Classes from Requirements & Building Entity Classes #2

---
+++ c#/ACM/ACM.BL/Customer.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        public string CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if(!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+
+        public static int InstanceCount { get; set; }
+
+    }
+}
---
+++ c#/ACM/Tests/ACM.BLTest/CustomerTest.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using ACM.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACM.BLTest
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            Customer customer = new Customer()
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins"
+            };
+            string expected = "Baggins, Bilbo";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            Customer customer = new Customer()
+            {
+                LastName = "Baggins"
+            };
+            string expected = "Baggins";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            Customer customer = new Customer()
+            {
+                FirstName = "Bilbo",
+            };
+            string expected = "Bilbo";
+
+            string actual = customer.FullName;
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            Customer c1 = new Customer()
+            {
+                FirstName = "Bilbo",
+            };
+            Customer.InstanceCount++;
+
+            Customer c2 = new Customer()
+            {
+                FirstName = "Frodo",
+            };
+            Customer.InstanceCount++;
+
+            Customer c3 = new Customer()
+            {
+                FirstName = "Rosie",
+            };
+            Customer.InstanceCount++;
+
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
+    }
+}
