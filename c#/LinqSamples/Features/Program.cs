2bd2fa144528e648aa3737bae3f954f3f725dccb
JS_MM <jsmm0202@gmail.com>
2020-03-24 17:18:48 +0200

    LINQ and C# module

--- c#/LinqSamples/Features/Program.cs
+++ c#/LinqSamples/Features/Program.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Features
 {
@@ -6,16 +7,28 @@ namespace Features
     {
         public static void Main(string[] args)
         {
-            Employee[] developers = new Employee[]
+            IEnumerable<Employee> developers = new Employee[]
             {
                 new Employee { Id = 1, Name = "Scott" },
                 new Employee { Id = 2, Name = "Chris" },
             };
 
-            List<Employee> sales = new List<Employee>()
+            IEnumerable<Employee> sales = new List<Employee>()
             {
                 new Employee { Id = 3, Name = "Alex" },
             };
+
+            var query = developers.Where(e => e.Name.Length == 5).OrderBy(e => e.Name);
+
+            var query2 = from developer in developers
+                         where developer.Name.Length == 5
+                         orderby developer.Name
+                         select developer;
+
+            foreach (var employee in query2)
+            {
+                System.Console.WriteLine(employee.Name);
+            }
         }
     }
 }
