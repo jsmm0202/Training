3832b51aa2438b9be1fdf5c2dff86983a8f863dd
JS_MM <jsmm0202@gmail.com>
2020-03-31 22:46:31 +0300

    Creating automated tests for file access code #11

--- c#/DataProcessor#11/DataProcessor/CsvFileProcessor.cs
+++ c#/DataProcessor#11/DataProcessor/CsvFileProcessor.cs
@@ -6,25 +6,34 @@ using System.Text;
 using System.Threading.Tasks;
 using CsvHelper;
 using CsvHelper.Configuration;
+using System.IO.Abstractions;
 
 namespace DataProcessor
 {
-    class CsvFileProcessor
+    public class CsvFileProcessor
     {
+        private readonly IFileSystem _fileSystem;
+
         public string InputFilePath { get; }
         public string OutputFilePath { get; }
 
         public CsvFileProcessor(string inputFilePath, string outputFilePath)
+            : this(inputFilePath, outputFilePath, new FileSystem()) { }
+
+
+        public CsvFileProcessor(string inputFilePath, string outputFilePath,
+                                IFileSystem fileSystem)
         {
             InputFilePath = inputFilePath;
             OutputFilePath = outputFilePath;
+            _fileSystem = fileSystem;
         }
 
         public void Process()
         {
-            using (StreamReader input = File.OpenText(InputFilePath))
+            using (StreamReader input = _fileSystem.File.OpenText(InputFilePath))
             using (CsvReader csvReader = new CsvReader(input))
-            using (StreamWriter output = File.CreateText(OutputFilePath))
+            using (StreamWriter output = _fileSystem.File.CreateText(OutputFilePath))
             using (var csvWriter = new CsvWriter(output))
             {
                 csvReader.Configuration.TrimOptions = TrimOptions.Trim;
--- c#/DataProcessor#11/DataProcessor/TextFileProcessor.cs
+++ c#/DataProcessor#11/DataProcessor/TextFileProcessor.cs
@@ -1,22 +1,30 @@
 ﻿using System.IO;
+using System.IO.Abstractions;
 
 namespace DataProcessor
 {
     public class TextFileProcessor
     {
+        private readonly IFileSystem _fileSystem;
+
         public string InputFilePath { get; }
         public string OutputFilePath { get; }
 
         public TextFileProcessor(string inputFilePath, string outputFilePath)
+            : this(inputFilePath, outputFilePath, new FileSystem()) { }
+
+        public TextFileProcessor(string inputFilePath, string outputFilePath, 
+            IFileSystem fileSystem)
         {
             InputFilePath = inputFilePath;
             OutputFilePath = outputFilePath;
+            _fileSystem = fileSystem;
         }
 
         public void Process()
         {
-            using (var inputStreamReader = new StreamReader(InputFilePath))            
-            using (var outputStreamWriter = new StreamWriter(OutputFilePath))
+            using (var inputStreamReader = _fileSystem.File.OpenText(InputFilePath))            
+            using (var outputStreamWriter = _fileSystem.File.CreateText(OutputFilePath))
             {
                 var currentLineNumber = 1;
                 while (!inputStreamReader.EndOfStream)
