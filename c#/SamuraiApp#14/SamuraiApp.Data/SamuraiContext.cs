58425515ce6a41d6c00072919fd6823998560b94
JS_MM <jsmm0202@gmail.com>
2020-04-05 20:24:25 +0300

    Mapping many to many and one to one relationships #14

--- c#/SamuraiApp#14/SamuraiApp.Data/SamuraiContext.cs
+++ c#/SamuraiApp#14/SamuraiApp.Data/SamuraiContext.cs
@@ -3,16 +3,22 @@ using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
-    public class SamuraiContext:DbContext
+  public class SamuraiContext : DbContext
+  {
+    public DbSet<Samurai> Samurais { get; set; }
+    public DbSet<Quote> Quotes { get; set; }
+    public DbSet<Clan> Clans { get; set; }
+    public DbSet<Battle> Battles { get; set; }
+   
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        public DbSet<Samurai> Samurais { get; set; }
-        public DbSet<Quote> Quotes { get; set; }
-        public DbSet<Clan> Clans { get; set; }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-             optionsBuilder.UseSqlServer(
-                 "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
-        }
+      optionsBuilder.UseSqlServer(
+          "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
     }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
+      modelBuilder.Entity<Horse>().ToTable("Horses");
+    }
+  }
 }
