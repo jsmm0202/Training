5b196e2fe98109407638c00a4868df7d8f5ee3b4
JS_MM <jsmm0202@gmail.com>
2020-04-06 19:05:00 +0300

    Using EF core with ASP.NET core #14

--- c#/SamuraiApp#14/SamuraiApp.Data/SamuraiContext.cs
+++ c#/SamuraiApp#14/SamuraiApp.Data/SamuraiContext.cs
@@ -1,39 +1,29 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore.Logging;
 using SamuraiApp.Domain;
 
 namespace SamuraiApp.Data
 {
   public class SamuraiContext : DbContext
   {
+
+    public SamuraiContext(DbContextOptions<SamuraiContext> options)
+      :base(options)
+    {
+      ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+    }
+
     public DbSet<Samurai> Samurais { get; set; }
     public DbSet<Quote> Quotes { get; set; }
     public DbSet<Clan> Clans { get; set; }
     public DbSet<Battle> Battles { get; set; }
+    public DbSet<SamuraiBattleStat> SamuraiBattleStats { get; set; }
 
-    public static readonly ILoggerFactory ConsoleLoggerFactory
-      = LoggerFactory.Create(builder =>
-      {
-        builder
-         .AddFilter((category, level) =>
-             category == DbLoggerCategory.Database.Command.Name
-             && level == LogLevel.Information)
-         .AddConsole();
-      });
-
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-      optionsBuilder
-         .UseLoggerFactory(ConsoleLoggerFactory)
-         .EnableSensitiveDataLogging()
-         .UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = SamuraiAppData");
-    }
-   
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
       modelBuilder.Entity<SamuraiBattle>().HasKey(s => new { s.SamuraiId, s.BattleId });
       modelBuilder.Entity<Horse>().ToTable("Horses");
+      modelBuilder.Entity<SamuraiBattleStat>().HasNoKey().ToView("SamuraiBattleStats");
     }
-
   }
 }
